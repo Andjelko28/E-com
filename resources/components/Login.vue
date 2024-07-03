20951f91a8466d61832b2b06d43754a85c55e800
Andjelko28 <105885932+Andjelko28@users.noreply.github.com>
2024-07-03 13:00:27 +0200

    "Updated Login and Register components with form templates, added Login route, and imported form styles in app.scss"

--- resources/components/Login.vue
+++ resources/components/Login.vue
@@ -1,6 +1,48 @@
 <template>
-
+    <div class="form-container">
+        <form>
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-md-12">
+                        <div class="form-group">
+                            <label for="exampleInputEmail1"
+                                >Email address</label
+                            >
+                            <input
+                                type="email"
+                                class="form-control"
+                                id="email"
+                                aria-describedby="emailHelp"
+                                placeholder="johndoe@gmail.com"
+                            />
+                        </div>
+                    </div>
+                </div>
+                <div class="row justify-content-center">
+                    <div class="col-md-12">
+                        <div class="form-group">
+                            <label for="password">Password</label>
+                            <input
+                                type="password"
+                                class="form-control"
+                                id="password"
+                                placeholder="Password"
+                            />
+                        </div>
+                    </div>
+                </div>
+                <div class="row justify-content-center mt-2">
+                    <div class="col-md-12 d-flex justify-content-between">
+                        <button type="submit" class="btn btn-primary btn-block">
+                            Submit
+                        </button>
+                        <button class="btn btn-secondary">Cancel</button>
+                    </div>
+                </div>
+            </div>
+        </form>
+    </div>
 </template>
 <script>
-export default {}
+export default {};
 </script>
--- resources/js/routes.js
+++ resources/js/routes.js
@@ -1,13 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Home from '../components/Home.vue';
 import Register from '../components/Register.vue';
+import Login from '../components/Login.vue';
 
 const routes = [
     {
         path: '/', component: Home
     },
     {
-        path:'/register',component: Register
+        path: '/register', component: Register
+    },
+    {
+        path: '/login', component: Login
     }
 ];
 
