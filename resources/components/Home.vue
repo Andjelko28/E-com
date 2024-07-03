d255e22a42122c4bfedfa72c6686802042592768
Andjelko28 <105885932+Andjelko28@users.noreply.github.com>
2024-07-03 12:10:04 +0200

    "Updated bootstrap/app.php to include API routes, modified Home.vue component, and added Dropdown component and styles in app.scss"

--- resources/components/Home.vue
+++ resources/components/Home.vue
@@ -1,27 +1,41 @@
 <template>
     <div class="layout">
         <nav class="d-flex justify-content-between align-items-center p-2">
-            <router-link to="/" class="a-style-none"><h1 class="h1-font">BBBuy</h1></router-link>
+            <router-link to="/" class="a-style-none"
+                ><h1 class="h1-font">BBBuy</h1></router-link
+            >
             <div>
                 <ul class="list-style-none d-flex gap-2 m-0">
-                    <li class="btn btn-light fs-5">Log In</li>
-                    <li class="btn btn-light fs-5"><router-link to="/register" class="a-style-none">Sign Up</router-link></li>
+                    <li class="btn btn-light fs-5">
+                        <router-link to="/login" class="a-style-none"
+                            >Log In</router-link
+                        >
+                    </li>
+                    <li class="btn btn-light fs-5">
+                        <router-link to="/register" class="a-style-none"
+                            >Sign Up</router-link
+                        >
+                    </li>
                 </ul>
             </div>
         </nav>
         <aside>
-            <ul class="list-style-none">
-                <li>Products</li>
-                <li>Categories</li>
-                <li>Brands</li>
-            </ul>
+            <div>
+                <a class="btn btn-secondary m-2 dropdown-button">All Products</a>
+                <Dropdown :fetchData="null">Categories</Dropdown>
+                <Dropdown :fetchData="null">Brand</Dropdown>
+            </div>
         </aside>
-        <main>
-
-        </main>
+        <main></main>
     </div>
 </template>
 
 <script>
-export default {};
+import Dropdown from "./helpers/Dropdown.vue";
+
+export default {
+    components:{
+        Dropdown
+    }
+};
 </script>
---
+++ resources/components/helpers/Dropdown.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="dropdown m-2">
+        <button :for="label" class="btn btn-secondary dropdown-toggle dropdown-button"><slot></slot></button>
+        <div class="dropdown-menu">
+            <a href="" class="dropdown-item">Link 1</a>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        label: String,
+    },
+    fetchData: {
+        type: Function,
+    },
+    data() {
+        return {
+            items: [],
+            selectedValue: null,
+        };
+    },
+    mounted() {
+        //  this.fetcData().then((response) => {
+        //      this.items = response.data;
+        //  });
+    },
+};
+</script>
+
